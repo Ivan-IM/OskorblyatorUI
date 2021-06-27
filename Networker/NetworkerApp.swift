2f796ae1151779dab5a1920bc11b740a88e5cdc5
iMarishin <move_up@icloud.com>
2021-06-27 15:53:46 +0500

    Initial Commit

---
+++ Networker/NetworkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkerApp.swift
+//  Networker
+//
+//  Created by Иван Маришин on 27.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct NetworkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
